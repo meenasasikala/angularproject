2281fdee1b3d99740f071a51ed2c41a359cedaa3
Sasidurga@123 <meenarajendran31@gmail.com>
2022-09-19 08:42:58 +0530

    angular

---
+++ angular/src/app/header/header.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  twoway:any;
+  isdisabled:boolean=true;
+
+  constructor() { }
+
+
+  ngOnInit(): void {
+  }
+  
+  onbtn(){
+    this.isdisabled=true;
+    this.twoway='';
+  }
+  oninput(event:any){
+    this.isdisabled=false;
+    console.log(event)
+  }
+  
+}
