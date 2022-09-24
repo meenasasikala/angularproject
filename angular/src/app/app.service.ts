68cc9a1db1422da19810aadcdd14e80aaea1cad9
Sasidurga@123 <meenarajendran31@gmail.com>
2022-09-24 17:31:23 +0530

    service put

--- angular/src/app/app.service.ts
+++ angular/src/app/app.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {HttpClient}  from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppService {
 
-  constructor() { }
+  constructor(private httpclient:HttpClient) { }
+  adduser(){
+    return this.httpclient.post('http://localhost:3000/users',{'id':1})
+  }
 }
